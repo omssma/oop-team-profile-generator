0bb23582cfbdb7be6b3e5886e01162c435aaa10c
omssma <omaralkhanbashi92@gmail.com>
2021-10-31 18:02:45 -0400

    First commit

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,56 @@
+const Employee = require("../lib/Employee");
+
+// Create Jest test for employee
+test("Can instantiate Employee instance", () => {
+  const e = new Employee();
+  expect(typeof(e)).toBe("object");
+});
+
+// Jest test for name
+test("Can set name via constructor arguments", () => {
+  const name = "Omar";
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
+});
+
+// Jest test for ID number
+test("Can set id via constructor argument", () => {
+  const testValue = 100;
+  const e = new Employee("Foo", testValue);
+  expect(e.id).toBe(testValue);
+});
+
+// Jest test for email address
+test("Can set email via constructor argument", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("Foo", 1, testValue);
+  expect(e.email).toBe(testValue);
+});
+
+// Jest test to test the value of name
+test("Can get name via getName()", () => {
+  const testValue = "Omar";
+  const e = new Employee(testValue);
+  expect(e.getName()).toBe(testValue);
+});
+
+// Jest test to test the value of ID number
+test("Can get id via getId()", () => {
+  const testValue = 100;
+  const e = new Employee("Foo", testValue);
+  expect(e.getId()).toBe(testValue);
+});
+
+// Jest test to test the value of email address
+test("Can get email via getEmail()", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("Foo", 1, testValue);
+  expect(e.getEmail()).toBe(testValue);
+});
+
+// Jest test to test the value of role
+test("getRole() should return \"Employee\"", () => {
+  const testValue = "Employee";
+  const e = new Employee("Omar", 1, "test@test.com");
+  expect(e.getRole()).toBe(testValue);
+});
---
+++ __tests__/Intern.test.js
@@ -0,0 +1,22 @@
+const Intern = require("../lib/Intern");
+
+
+// Jest test to test the value of school
+test("Can set school via constructor", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+// Jest test to tes the value of role
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "UCLA";
+  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
